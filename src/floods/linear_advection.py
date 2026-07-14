a1ae41451841a15bdb73740da5c5da12c3d4776f
zachpmurph <zachpmurph@gmail.com>
2026-07-13 22:40:29 -0700

    Added source term into dif eq solver/model

--- src/floods/linear_advection.py
+++ src/floods/linear_advection.py
@@ -2,22 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parameters
-c = -1.0          # wave speed
-L = 10.0         # domain length
+c = 1.0          # wave speed
+L = 20.0         # domain length
 Nx = 200         # number of cells
 dx = L / Nx
 CFL = 0.9        # Courant number (must be <= 1)
 dt = CFL * dx / np.abs(c)
-T_final = 5.0    # total simulation time
+T_final = 2.0    # total simulation time
 Nt = int(T_final / dt)
-center = 8.0
+center = 3.0
 nu = c * dt / dx
+def r(x, t):
+    if (t >=0 and t < 5):
+        return 0.1 * dt
+    return 0
 
 # Grid
 x = np.linspace(0, L, Nx)
 
 # Initial condition: a Gaussian pulse
-u = np.exp(-((x - center)**2) / 0.2)
+#u = np.exp(-((x - center)**2) / 0.2)
+u = np.zeros(len(x))
 u_initial = u.copy()
 
 # Time stepping with upwind scheme
@@ -29,11 +34,11 @@ for n in range(Nt):
     else:
         u_new[:-1] = u[:-1] - nu * (u[1:] - u[:-1])
         u_new[-1] = 0.0   # right boundary is now inflow
-    u = u_new
+    u = u_new + (r(x, n *dt))
 
 # Plot
 plt.plot(x, u_initial, label='Initial')
 plt.plot(x, u, label=f'After t = {T_final}', ls = '--')
-plt.axvline(center + c * T_final, linestyle='--', label='Expected position')
+#plt.axvline(center + c * T_final, linestyle='--', label='Expected position')
 plt.legend(); plt.xlabel('x'); plt.ylabel('u')
 plt.show()
